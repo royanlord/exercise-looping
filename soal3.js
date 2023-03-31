e437c480ba63200a97400f640729fd144c565fdf
royanlord <royanfarid32@gmail.com>
2023-03-31 10:29:16 +0700

    add exercise 3

---
+++ soal3.js
@@ -0,0 +1,8 @@
+// soal 3
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} adalah bilangan genap`);
+    } else {
+        console.log(`${i} adalah bilangan ganjil`);
+    }
+}
