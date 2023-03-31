5f1dd8092f3fa5cdf0303b8cbb920a324d61c31d
royanlord <royanfarid32@gmail.com>
2023-03-31 13:32:47 +0700

    modified soal5.js

--- soal5.js
+++ soal5.js
@@ -1,9 +1,9 @@
 // soal 5
 let correct = "Impact Byte";
-let invalid = null;
+let result = null;
 let say = "Jelaskan kepanjangan dari nama IB?";
 
-while (invalid !== correct) {
-    invalid = prompt("Jelaskan kepanjangan dari nama IB?");
+while (result !== correct) {
+    result = prompt("Jelaskan kepanjangan dari nama IB?");
 }
 alert("Selamat jawaban kamu benar!");
