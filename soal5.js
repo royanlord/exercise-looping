23b0e104c78f0de124bcaf6ebe50402226100802
royanlord <royanfarid32@gmail.com>
2023-03-31 10:29:51 +0700

    add exercise 5

---
+++ soal5.js
@@ -0,0 +1,9 @@
+// soal 5
+let correct = "Impact Byte";
+let invalid = null;
+let say = "Jelaskan kepanjangan dari nama IB?";
+
+while (invalid !== correct) {
+    invalid = prompt("Jelaskan kepanjangan dari nama IB?");
+}
+alert("Selamat jawaban kamu benar!");
